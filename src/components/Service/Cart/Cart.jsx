04d97ed8eff1154fa1585a51772ed75bb6e21104
Showman_Das <Showmandas3216@gmail.com>
2023-04-08 00:37:39 +0600

    apply condition rendering

--- src/components/Service/Cart/Cart.jsx
+++ src/components/Service/Cart/Cart.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 const Cart = ({cart, handleRemoveButton}) => {
+    let message;
+    if(cart.length===0){
+         message=<h1>Pls buy something</h1>
+    }
     return (
         <div>
-            <h3>{cart.length}</h3>
+            {/* <h3>{cart.length}</h3> */}
+            {message}
             {
                 cart.map(tshirt=>
                     <p className='bg-accent mb-2'>{tshirt.name} <button className='bg-primary p-2 px-2' onClick={()=>handleRemoveButton(tshirt._id)}>X</button></p> )
+            }
+            {
+                cart.length===2 && <p>wow! Double</p>
             }
         </div>
     );
